fe6fa052df864f20222a38ab5bd895548fadda8f
bbutz <bbutz@ransom-briggss-macbook-pro-15.local>
2010-02-08 21:38:47 -0600

    HW2: refactoring main into functions based on sections

--- WhatATool/WhatATool.m
+++ WhatATool/WhatATool.m
@@ -1,8 +1,6 @@
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
+void PrintPathInfo() {
 	NSLog(@"===========");
 	NSLog(@"Section One");
 	NSLog(@"===========");
@@ -15,7 +13,9 @@ int main (int argc, const char * argv[]) {
 	for (NSString *component in pathComponents) {
 		NSLog(component);
 	}
-	
+}
+
+void PrintProcessInfo() {
 	NSLog(@"===========");
 	NSLog(@"Section Two");
 	NSLog(@"===========");
@@ -23,38 +23,42 @@ int main (int argc, const char * argv[]) {
 	NSLog(@"Process Name '%@' Process ID: '%d'",
 		  [[NSProcessInfo processInfo] processName],
 		  [[NSProcessInfo processInfo] processIdentifier]);
+}
 
+void PrintBookmarkInfo() {
 	NSLog(@"=============");
 	NSLog(@"Section Three");
 	NSLog(@"=============");
 	
 	NSMutableDictionary *dict = 
-		[NSMutableDictionary dictionaryWithObjectsAndKeys:
-		 [NSURL URLWithString:@"http://www.stanford.edu"], @"Stanford Univeristy",
-		 [NSURL URLWithString:@"http://www.apple.com"], @"Apple",
-		 [NSURL URLWithString:@"http://cs193p.stanford.edu"], @"CS193P",
-		 [NSURL URLWithString:@"http://itunes.stanford.edu"], @"Stanford on iTunes U",
-		 [NSURL URLWithString:@"http://stanfordshop.com"], @"Stanford Mail",
-		 nil];
+	[NSMutableDictionary dictionaryWithObjectsAndKeys:
+	 [NSURL URLWithString:@"http://www.stanford.edu"], @"Stanford Univeristy",
+	 [NSURL URLWithString:@"http://www.apple.com"], @"Apple",
+	 [NSURL URLWithString:@"http://cs193p.stanford.edu"], @"CS193P",
+	 [NSURL URLWithString:@"http://itunes.stanford.edu"], @"Stanford on iTunes U",
+	 [NSURL URLWithString:@"http://stanfordshop.com"], @"Stanford Mail",
+	 nil];
 	
 	for (id key in dict) {
 		if ([key hasPrefix:@"Stanford"]) {
 			NSLog(@"Key: '%@' URL: '%@'", key, [dict valueForKey:key]);
 		}
-	}
-	
+	}	
+}
+
+void PrintIntrospectionInfo() {
 	NSLog(@"============");
 	NSLog(@"Section Four");
 	NSLog(@"============");
 	
 	NSMutableArray *array = 
-		[NSMutableArray arrayWithObjects:
-			@"STRING, INSTANTLY, IN 20 SECONDS FLAT",
-			[NSURL URLWithString:@"http://www.google.com"],
-			[NSDictionary dictionaryWithObjectsAndKeys: @"key", @"value", nil],
-			[NSMutableString stringWithString:@"IMMA MUTANT!"],
-			[NSArray arrayWithObjects: @"1", @"2", @"300", nil],
-			nil];
+	[NSMutableArray arrayWithObjects:
+	 @"STRING, INSTANTLY, IN 20 SECONDS FLAT",
+	 [NSURL URLWithString:@"http://www.google.com"],
+	 [NSDictionary dictionaryWithObjectsAndKeys: @"key", @"value", nil],
+	 [NSMutableString stringWithString:@"IMMA MUTANT!"],
+	 [NSArray arrayWithObjects: @"1", @"2", @"300", nil],
+	 nil];
 	
 	for(id element in array) {
 		NSLog(@"Class name: %@", [element className]);
@@ -69,7 +73,16 @@ int main (int argc, const char * argv[]) {
 				  [element performSelector:@selector(lowercaseString)]);
 		}
 		NSLog(@"=======================");
-	}
+	}	
+}
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	PrintPathInfo();			// Section 1
+	PrintProcessInfo();			// Section 2
+	PrintBookmarkInfo();		// Section 3
+	PrintIntrospectionInfo();	// Section 4
 	
     [pool drain];
     return 0;
